4e78ba5c07ab6a9b1668db27066e8f1e8b40e6ab
fengmnegchang <624926379@qq.com>
2017-08-18 22:25:53 +0800

    androidslidingmenu studio

---
+++ app/src/main/java/com/jeremyfeinstein/slidingmenu/lib/app/SlidingFragmentActivity.java
@@ -0,0 +1,255 @@
+package com.jeremyfeinstein.slidingmenu.lib.app;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewCompat;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
+public class SlidingFragmentActivity extends FragmentActivity implements SlidingActivityBase {
+
+	private SlidingActivityHelper mHelper;
+	private int statusBarColor;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mHelper = new SlidingActivityHelper(this);
+		mHelper.onCreate(savedInstanceState);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onPostCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		mHelper.onPostCreate(savedInstanceState);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#findViewById(int)
+	 */
+	@Override
+	public View findViewById(int id) {
+		View v = super.findViewById(id);
+		if (v != null)
+			return v;
+		return mHelper.findViewById(id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os
+	 * .Bundle)
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mHelper.onSaveInstanceState(outState);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#setContentView(int)
+	 */@SuppressLint("NewApi")
+	@Override
+	public void setContentView(int id) {
+		try {
+			
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				Window window = getWindow();
+				// 取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
+				window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+				// 需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+				window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+				// 设置状态栏颜色
+				window.setStatusBarColor(statusBarColor);
+				ViewGroup mContentView = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
+				View mChildView = mContentView.getChildAt(0);
+				if (mChildView != null) {
+					// 注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView
+					// 的第一个子 View . 预留出系统 View 的空间.
+					ViewCompat.setFitsSystemWindows(mChildView, true);
+				}
+			}
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+				ViewGroup contentView = (ViewGroup) findViewById(android.R.id.content);
+				View statusBarView = new View(this);
+				ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(this));
+				statusBarView.setBackgroundColor(statusBarColor);
+				contentView.addView(statusBarView, lp);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		setContentView(getLayoutInflater().inflate(id, null));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#setContentView(android.view.View)
+	 */
+	@Override
+	public void setContentView(View v) {
+		setContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#setContentView(android.view.View,
+	 * android.view.ViewGroup.LayoutParams)
+	 */
+	@Override
+	public void setContentView(View v, LayoutParams params) {
+		super.setContentView(v, params);
+		mHelper.registerAboveContentView(v, params);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#
+	 * setBehindContentView(int)
+	 */
+	public void setBehindContentView(int id) {
+		setBehindContentView(getLayoutInflater().inflate(id, null));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#
+	 * setBehindContentView(android.view.View)
+	 */
+	public void setBehindContentView(View v) {
+		setBehindContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#
+	 * setBehindContentView(android.view.View,
+	 * android.view.ViewGroup.LayoutParams)
+	 */
+	public void setBehindContentView(View v, LayoutParams params) {
+		mHelper.setBehindContentView(v, params);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#getSlidingMenu
+	 * ()
+	 */
+	public SlidingMenu getSlidingMenu() {
+		return mHelper.getSlidingMenu();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#toggle()
+	 */
+	public void toggle() {
+		mHelper.toggle();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#showAbove()
+	 */
+	public void showContent() {
+		mHelper.showContent();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#showBehind()
+	 */
+	public void showMenu() {
+		mHelper.showMenu();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#showSecondaryMenu
+	 * ()
+	 */
+	public void showSecondaryMenu() {
+		mHelper.showSecondaryMenu();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivityBase#
+	 * setSlidingActionBarEnabled(boolean)
+	 */
+	public void setSlidingActionBarEnabled(boolean b) {
+		mHelper.setSlidingActionBarEnabled(b);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onKeyUp(int, android.view.KeyEvent)
+	 */
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		boolean b = mHelper.onKeyUp(keyCode, event);
+		if (b)
+			return b;
+		return super.onKeyUp(keyCode, event);
+	}
+
+	public int getStatusBarColor() {
+		return statusBarColor;
+	}
+
+	public void setStatusBarColor(int statusBarColor) {
+		this.statusBarColor = statusBarColor;
+	}
+
+	public static int getStatusBarHeight(Context context) {
+		int result = 0;
+		int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+		if (resourceId > 0) {
+			result = context.getResources().getDimensionPixelSize(resourceId);
+		}
+		return result;
+	}
+}
